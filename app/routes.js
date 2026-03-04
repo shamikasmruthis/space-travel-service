32857f6a24c38b31ddd14636fda7f11cb1ad8d12
Shamixx <shamikasmruthi@gmail.com>
2026-03-04 14:53:22 +0000

    branching-logic-added

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,18 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/check-dest', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var Destination = req.session.data['whereDoYouLive']
+
+  // Check whether the variable matches a condition
+  if (Destination == "Moon"){
+    // Send user to next page
+    res.redirect('/confirmation-moon')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/confirmation-mars')
+  }
+
+})
